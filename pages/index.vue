f2fe59cf12d7791db12bf5ac1bf506eca08dd2ff
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-10-01 11:57:12 +0200

    Component not used and bugged

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
-    <FadeInScroll>
-        <h1>
-            <span
-                class="block font-display text-base font-semibold text-neutral-950"
-                >Contact us</span
-            ><span class="sr-only"> - </span
-            ><span
-                class="mt-6 block max-w-5xl font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-6xl"
-                >Let’s work together</span
-            >
-        </h1>
-        <div class="mt-6 max-w-3xl text-xl text-neutral-600">
-            <p>We can’t wait to hear from you.</p>
-        </div>
-    </FadeInScroll>
+    <h1>
+        <span
+            class="font-display text-neutral-950 block text-base font-semibold"
+            >Contact us</span
+        ><span class="sr-only"> - </span
+        ><span
+            class="font-display text-neutral-950 mt-6 block max-w-5xl text-5xl font-medium tracking-tight [text-wrap:balance] sm:text-6xl"
+            >Let’s work together</span
+        >
+    </h1>
+    <div class="text-neutral-600 mt-6 max-w-3xl text-xl">
+        <p>We can’t wait to hear from you.</p>
+    </div>
 </template>
