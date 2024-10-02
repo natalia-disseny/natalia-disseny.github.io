6cd92d207fa65647d30337375af8b6548236d9aa
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-10-02 14:04:05 +0200

    Hydration Vue error fixed on Footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,9 +1,4 @@
 <script setup>
-const appConfig = useAppConfig()
-defineProps({
-    link: String,
-})
-
 const licenseImages = [
     'https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1',
     'https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1',
@@ -20,22 +15,19 @@ const currentDate = new Date()
             class="border-neutral-950/10 mb-20 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t pt-12">
             <Logo />
             <div class="text-xs">
-                <p xmlns:cc="http://creativecommons.org/ns#">
-                    © {{ currentDate.getFullYear() }} - This work is licensed
-                    under
-                    <TextLink
-                        link="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1"
-                        target="_blank"
-                        >CC BY-NC-SA 4.0
+                © {{ currentDate.getFullYear() }} - This work is licensed under
+                <TextLink
+                    link="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1"
+                    target="_blank"
+                    >CC BY-NC-SA 4.0
 
-                        <div class="mt-2 flex justify-end gap-x-2">
-                            <img
-                                v-for="image in licenseImages"
-                                class="h-6"
-                                :src="image" />
-                        </div>
-                    </TextLink>
-                </p>
+                    <div class="mt-2 flex justify-end gap-x-2">
+                        <img
+                            v-for="image in licenseImages"
+                            class="h-6"
+                            :src="image" />
+                    </div>
+                </TextLink>
             </div>
         </div>
     </footer>
