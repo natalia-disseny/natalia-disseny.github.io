0a920fe8f21a438fecd62d0a20668954fabb0e3f
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-10-04 14:09:50 +0200

    .env file for building in Github Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     css: ['~/assets/css/main.css'],
     devtools: { enabled: true },
+    axios: {
+        baseURL: process.env.BASE_URL,
+    },
 
     postcss: {
         plugins: {
