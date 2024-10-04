1f8316a9033a258e05d7ec82bd26b4581ea497e5
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-10-04 14:12:37 +0200

    bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     css: ['~/assets/css/main.css'],
     devtools: { enabled: true },
-    axios: {
-        baseURL: process.env.BASE_URL,
-    },
 
     postcss: {
         plugins: {
