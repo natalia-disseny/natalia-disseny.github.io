d7f9cd697d1c0f275187e9dd2a0a5abc8c25549e
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-10-18 12:06:33 +0200

    Contact form: message field grows with input

--- components/form/Textarea.vue
+++ components/form/Textarea.vue
@@ -35,7 +35,7 @@ const resizeTextarea = (event) => {
             class="border-neutral-300 bg-transparent focus:border-neutral-950 peer block w-full border px-6 pb-4 pt-12 text-base/6 text-gray-900 transition focus:border-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-50 group-first:rounded-t-2xl group-last:rounded-b-2xl"
             :name="name"
             v-model="value"></textarea>
-        <FormLabel :for="name" class="absolute peer-focus:top-12"
+        <FormLabel :for="name" class="absolute top-0 mt-7"
             >{{ placeholder }}
         </FormLabel>
     </div>
