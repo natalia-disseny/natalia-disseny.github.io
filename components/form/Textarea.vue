bd5e3aa071768e3efb9ac71ee53f543fd9b9a0fe
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-11-06 14:41:13 +0100

    Form fixes

--- components/form/Textarea.vue
+++ components/form/Textarea.vue
@@ -27,7 +27,7 @@ const resizeTextarea = (event) => {
     <div class="group relative z-0 transition-all focus-within:z-10">
         <textarea
             @keydown.enter="rows++"
-            @keydown="resizeTextarea"
+            @keyup="resizeTextarea"
             :rows="rows"
             v-bind="$attrs"
             :id="name"
@@ -35,7 +35,7 @@ const resizeTextarea = (event) => {
             class="border-neutral-300 bg-transparent focus:border-neutral-950 peer block w-full border px-6 pb-4 pt-12 text-base/6 text-gray-900 transition focus:border-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-50 group-first:rounded-t-2xl group-last:rounded-b-2xl"
             :name="name"
             v-model="value"></textarea>
-        <FormLabel :for="name" class="absolute top-0 mt-7"
+        <FormLabel :for="name" class="!top-[44px]"
             >{{ placeholder }}
         </FormLabel>
     </div>
