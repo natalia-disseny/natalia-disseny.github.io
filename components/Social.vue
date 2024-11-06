f995c182460a27a70bbe7f443434f2ecd8384447
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-11-06 14:31:21 +0100

    fontello added

--- components/Social.vue
+++ components/Social.vue
@@ -14,18 +14,11 @@ defineProps({
                 v-for="social in useAppConfig().contact.social"
                 :aria-label="social.label"
                 :class="{ '--onDark': onDark }"
-                class="btn-round transition"
+                class="btn-round text-2xl transition"
                 :href="social.url"
                 target="_blank"
-                ><svg
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                    class="fill-current h-6 w-6">
-                    <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        :d="social.svg"></path></svg
-            ></a>
+                ><i :class="`icon-${social.icon}`"></i>
+            </a>
         </div>
     </div>
 </template>
