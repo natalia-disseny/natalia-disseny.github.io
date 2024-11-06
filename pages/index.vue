87a5de65ce898d1c200911a4643c8f8ffc3940bc
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-11-06 14:58:41 +0100

    Home changed

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
+<script setup>
+import Intro from './index/intro.vue'
+import Goals from './index/goals.vue'
+import Services from './index/services.vue'
+</script>
+
 <template>
-    <h1>
-        <span
-            class="font-display text-neutral-950 block text-base font-semibold"
-            >Contact us</span
-        ><span class="sr-only"> - </span
-        ><span
-            class="font-display text-neutral-950 mt-6 block max-w-5xl text-5xl font-medium tracking-tight [text-wrap:balance] sm:text-6xl"
-            >Let’s work together</span
-        >
-    </h1>
-    <div class="text-neutral-600 mt-6 max-w-3xl text-xl">
-        <p>We can’t wait to hear from you.</p>
-    </div>
+    <Title>{{
+        $t('about.button') + ' - ' + useAppConfig().contact.company
+    }}</Title>
+
+    <Intro />
+    <Goals />
+    <Services />
+    <ContactBox />
 </template>
