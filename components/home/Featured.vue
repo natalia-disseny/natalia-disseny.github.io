70d27619c16781c4637f6e1a22052c0fe7cf38df
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-11-22 14:39:19 +0100

    New home

---
+++ components/home/Featured.vue
@@ -0,0 +1,53 @@
+<script setup>
+const features = [
+    {
+        name: 'Development',
+        description:
+            'Tailored websites that combine unique designs and robust functionality to perfectly reflect your brand and meet your business goals.',
+        img: 'icon-coding.png',
+    },
+    {
+        name: 'Design',
+        description:
+            'Visual solutions to enhance your brand identity, from memorable logos to cohesive design elements that elevate your marketing materials.',
+        img: 'icon-design.png',
+    },
+    {
+        name: 'Consulting',
+        description:
+            'Expert guidance to optimize your online presence, improve user engagement, and drive results through targeted, innovative strategies.',
+        img: 'icon-consulting.png',
+    },
+]
+</script>
+
+<template>
+    <div class="text-center">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:max-w-none">
+                <dl
+                    class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                    <div
+                        v-for="(feature, index) in features"
+                        :key="feature.name"
+                        class="flex flex-col">
+                        <dt
+                            class="mb-4 font-display text-xl font-bold uppercase text-secondary-900">
+                            <img
+                                class="mx-auto mb-10 w-28"
+                                :src="'/home/' + feature.img"
+                                alt="Presentation icons created by Freepik - https://www.flaticon.com/free-icons/presentation" />
+                            {{ $t(`home.features[${index}].name`) }}
+                        </dt>
+                        <dd
+                            class="mt-1 flex flex-auto flex-col text-base/7 text-primary-600">
+                            <p class="flex-auto">
+                                {{ $t(`home.features[${index}].description`) }}
+                            </p>
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/home/HeroSection.vue
@@ -0,0 +1,62 @@
+<script setup>
+const localePath = useLocalePath()
+</script>
+
+<template>
+    <div class="relative isolate">
+        <div class="overflow-hidden">
+            <div class="mx-auto max-w-7xl px-6 pb-32 lg:px-8">
+                <div
+                    class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+                    <div
+                        class="relative w-full lg:max-w-xl lg:shrink-0 xl:max-w-2xl">
+                        <h1
+                            class="text-gray-900 text-pretty text-5xl font-semibold tracking-tight sm:text-7xl">
+                            {{ $t('home.headline') }}
+                        </h1>
+                        <p
+                            class="text-gray-500 mt-8 text-pretty text-lg font-medium sm:max-w-md sm:text-xl/8 lg:max-w-none">
+                            {{ $t('home.subheadline') }}
+                        </p>
+                        <div class="mt-10 flex items-center gap-x-6">
+                            <PrimaryButton
+                                @click="menuIsVisible = false"
+                                :to="localePath('/contact')"
+                                title="Contact"
+                                class="hidden sm:inline"
+                                :onDark>
+                                {{ $t('contact.cta') }}
+                            </PrimaryButton>
+                        </div>
+                    </div>
+                    <div
+                        class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                        <div
+                            class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                            <HomeHeroImage
+                                image="hero_1.jpg"
+                                title="Photo by Andrew Neel: https://www.pexels.com/photo/woman-in-gray-jacket-sitting-beside-desk-2682452/" />
+                        </div>
+                        <div
+                            class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                            <HomeHeroImage
+                                image="hero_2.jpg"
+                                title="Photo by Leeloo The First from Pexels: https://www.pexels.com/photo/pictures-of-brand-strategy-and-design-7598018/" />
+                            <HomeHeroImage
+                                image="hero_3.jpg"
+                                title="Photo by picjumbo.com: https://www.pexels.com/photo/notebook-beside-the-iphone-on-table-196644/" />
+                        </div>
+                        <div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+                            <HomeHeroImage
+                                image="hero_4.jpg"
+                                title="Photo by Flo Dahm: https://www.pexels.com/photo/turned-on-gray-laptop-computer-on-table-699459/" />
+                            <HomeHeroImage
+                                image="hero_5.jpg"
+                                title="Photo by Roman Pohorecki: https://www.pexels.com/photo/hand-on-top-of-paper-17845/" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
