a12afc3a4f1054d8966b7d55a18d6fa2a3798af2
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-11-22 09:43:39 +0100

    Refactor on blog

--- pages/blog/latest.vue
+++ pages/blog/latest.vue
@@ -7,42 +7,7 @@ const articles = await queryContent('blog').sort({ date: -1 }).limit(3).find()
 <template>
     <div class="mb-8 flex flex-col gap-x-8 gap-y-8 lg:flex-row">
         <div class="group flex basis-2/3 flex-col justify-between">
-            <div>
-                <div class="relative">
-                    <NuxtLink v-if="articles[0]._path" :to="articles[0]._path">
-                        <div
-                            class="absolute -left-2 top-4 z-10 rounded-md bg-secondary-900 px-2 py-1 text-xs uppercase tracking-wider text-white shadow-sm">
-                            {{ articles[0].type }}
-                        </div>
-                        <div class="overflow-hidden rounded-3xl bg-coral-900">
-                            <img
-                                loading="lazy"
-                                class="w-full object-cover opacity-90 transition duration-500 motion-safe:group-hover:scale-105"
-                                :src="articles[0]._path + '.jpg'"
-                                :alt="articles[0].imageTitle"
-                                :title="articles[0].title" />
-                        </div>
-                    </NuxtLink>
-                </div>
-                <p class="my-2 font-body text-base text-secondary-900">
-                    {{ articles[0].date }}
-                </p>
-                <h2 class="font-display text-lg font-bold">
-                    {{ articles[0].title }}
-                </h2>
-                <p class="my-4">{{ articles[0].description }}</p>
-            </div>
-            <div class="flex justify-end">
-                <NuxtLink
-                    v-if="articles[0]._path"
-                    :to="articles[0]._path"
-                    class="btn-primary">
-                    Llegir més
-                    <span class="material-symbols-rounded ml-2 align-bottom">
-                        read_more
-                    </span>
-                </NuxtLink>
-            </div>
+            <BlogArticle :post="articles[0]" main-post />
         </div>
 
         <div class="flex basis-1/3 flex-col justify-between gap-y-8">
--- pages/blog/single.vue
+++ pages/blog/single.vue
@@ -1,5 +1,9 @@
 <script setup>
 defineProps({
+    mainPost: {
+        default: false,
+        type: Boolean,
+    },
     post: Object,
 })
 </script>
@@ -8,32 +12,52 @@ defineProps({
         <div class="relative">
             <NuxtLink v-if="post._path" :to="post._path">
                 <div
-                    class="absolute -left-2 top-4 z-10 rounded-md bg-secondary-900 px-2 py-1 text-xs uppercase tracking-wider text-white shadow-sm">
+                    class="absolute -left-2 top-4 z-10 rounded-md px-2 py-1 text-xs uppercase tracking-wider text-white shadow-sm"
+                    :class="[
+                        post.type == 'article'
+                            ? 'bg-secondary-900'
+                            : 'bg-coral-900',
+                    ]">
                     {{ post.type }}
                 </div>
                 <div class="overflow-hidden rounded-3xl bg-coral-900">
                     <img
                         loading="lazy"
-                        class="h-[200px] w-full object-cover opacity-90 transition duration-500 motion-safe:group-hover:scale-105"
+                        :class="{ 'h-[200px]': !mainPost }"
+                        class="w-full object-cover opacity-90 transition duration-500 motion-safe:group-hover:scale-105"
                         :src="post._path + '.jpg'"
                         :alt="post.imageTitle"
                         :title="post.title" />
                 </div>
             </NuxtLink>
         </div>
-        <div class="">
+        <div class="flex flex-col">
             <p class="my-2 font-body text-base text-secondary-900">
                 {{ post.date }}
             </p>
-            <div class="flex gap-x-2">
+            <div
+                class="flex justify-between gap-x-2"
+                :class="{ 'flex-col': mainPost }">
                 <h2 class="font-display text-lg font-bold">
                     {{ post.title }}
                 </h2>
+                <p v-if="mainPost" class="my-4">{{ post.description }}</p>
                 <NuxtLink
-                    v-if="post._path"
+                    v-if="!mainPost && post._path"
                     :to="post._path"
                     class="btn-primary w-12 self-center text-center">
                     <span class="material-symbols-rounded -ml-1.5 align-bottom">
+                        read_more
+                    </span>
+                </NuxtLink>
+            </div>
+            <div v-if="mainPost" class="flex justify-end">
+                <NuxtLink
+                    v-if="post._path"
+                    :to="post._path"
+                    class="btn-primary">
+                    Llegir més
+                    <span class="material-symbols-rounded ml-2 align-bottom">
                         read_more
                     </span>
                 </NuxtLink>
