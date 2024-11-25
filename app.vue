958fefcc7b445fb535509d570c89b0cec84e2b14
Natàlia Martínez <nsanchez.martinez@knowmadmood.com>
2024-11-25 12:16:44 +0100

    GTM

--- app.vue
+++ app.vue
@@ -32,6 +32,27 @@ const myGoogleFonts =
         <Html class="h-full bg-black text-base antialiased" :lang="locale" />
         <Title>{{ useAppConfig().contact.company }}</Title>
 
+        <Head>
+            <!-- Google Tag Manager -->
+            <script>
+                ;(function (w, d, s, l, i) {
+                    w[l] = w[l] || []
+                    w[l].push({
+                        'gtm.start': new Date().getTime(),
+                        event: 'gtm.js',
+                    })
+                    var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s),
+                        dl = l != 'dataLayer' ? '&l=' + l : ''
+                    j.async = true
+                    j.src =
+                        'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+                    f.parentNode.insertBefore(j, f)
+                })(window, document, 'script', 'dataLayer', 'GTM-K2F5GSQZ')
+            </script>
+            <!-- End Google Tag Manager -->
+        </Head>
+
         <Link rel="preconnect" href="https://fonts.googleapis.com" />
         <Link
             rel="preload"
@@ -46,6 +67,16 @@ const myGoogleFonts =
         </NoScript>
 
         <Body class="flex min-h-full flex-col">
+            <!-- Google Tag Manager (noscript) -->
+            <noscript
+                ><iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-K2F5GSQZ"
+                    height="0"
+                    width="0"
+                    style="display: none; visibility: hidden"></iframe
+            ></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+
             <LayoutHeader />
 
             <div
